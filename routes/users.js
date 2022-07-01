c8a7d3159c0eb70b5571c2140780d957e4f3c1fe
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-07-01 18:04:10 -0300

    Add: validating data received via POST

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,20 @@
 let NeDB = require('nedb');
 let db = new NeDB({
-
     filename:'users.db',
     autoload:true
-
 });
 
-module.exports = (app) => {
+module.exports = app => {
 
     let route = app.route('/users');
 
     route.get((req, res) => {
 
-        db.find({}).sort({name:1}).exec((err, users) => {
+        db.find({}).sort({name:1}).exec((err, users)=>{
 
-            if(err) {
-
+            if (err) {
                 app.utils.error.send(err, req, res);
-
-            }else {
+            } else {
 
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
@@ -31,15 +27,15 @@ module.exports = (app) => {
         });
 
     });
-    
+
     route.post((req, res) => {
+        
+        if (!app.utils.validator.user(app, req, res)) return false;
+        
+        db.insert(req.body, (err, user)=>{
 
-        db.insert(req.body, (err, user) => {
-
-            if(err) {
-
+            if (err) {
                 app.utils.error.send(err, req, res);
-
             } else {
 
                 res.status(200).json(user);
@@ -47,23 +43,19 @@ module.exports = (app) => {
             }
 
         });
-    
+
     });
 
     let routeId = app.route('/users/:id');
 
     routeId.get((req, res) => {
 
-        db.findOne({_id:req.params.id}).exec((err, user) => {
+        db.findOne({_id:req.params.id}).exec((err, user)=>{
 
-            if(err) {
-
+            if (err) {
                 app.utils.error.send(err, req, res);
-
             } else {
-
                 res.status(200).json(user);
-
             }
 
         });
@@ -71,39 +63,33 @@ module.exports = (app) => {
     });
 
     routeId.put((req, res) => {
+        
+        if (!app.utils.validator.user(app, req, res)) return false;
 
-        db.update({_id:req.params.id}, req.body, err => {
+        db.update({ _id: req.params.id }, req.body, err => {
 
-            if(err) {
-
+            if (err) {
                 app.utils.error.send(err, req, res);
-
             } else {
-
                 res.status(200).json(Object.assign(req.params, req.body));
-
             }
 
         });
 
     });
+    
+    routeId.delete((req, res)=>{
 
-    routeId.delete((req, res) => {
+        db.remove({ _id: req.params.id }, {}, err=>{
 
-        db.remove({_id:req.params.id}, {}, err => {
-
-            if(err) {
-
+            if (err) {
                 app.utils.error.send(err, req, res);
-
             } else {
-
                 res.status(200).json(req.params);
-
             }
 
         });
-                
+
     });
 
-}
+};
